8b1d343cb5e0fceca0b14b8787152f1bc06e9156
yoshiaki <yoshiaki110@gmail.com>
2025-02-27 15:08:42 +0900

    動画再生→画像保存

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, render_template, jsonify, Response
+from flask import Flask, request, jsonify, render_template
 import base64
 import os
-import json
 
+app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
-
-app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -18,14 +16,15 @@ def index():
 def upload_image():
     data = request.json
     image_data = data['image']
-    image_data = image_data.split(",")[1]  # remove the data URL header
+    file_name = data['fileName']
+    image_data = image_data.split(",")[1]  # data URLのヘッダを削除
     image_bytes = base64.b64decode(image_data)
 
-    file_path = os.path.join(UPLOAD_FOLDER, 'captured_image.png')
+    file_path = os.path.join(UPLOAD_FOLDER, file_name)
     with open(file_path, 'wb') as f:
         f.write(image_bytes)
 
-    return jsonify({'message': '画像が保存されました'}), 200 
+    return jsonify({'message': '画像が保存されました'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)

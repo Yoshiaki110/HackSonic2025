57f5450d37a2c93f853973924c1ccca38aa0dc55
yoshiaki <yoshiaki110@gmail.com>
2025-03-01 14:33:00 +0900

    とりあえず

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import base64
 import os
 
@@ -10,13 +10,22 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('skyway2.html')
+
+@app.route('/pub')
+def pub():
+    return render_template('pub.html')
+
+@app.route('/sub')
+def sub():
+    return render_template('sub.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
     data = request.json
     image_data = data['image']
     file_name = data['fileName']
+    finish = data['finish']
     image_data = image_data.split(",")[1]  # data URLのヘッダを削除
     image_bytes = base64.b64decode(image_data)
 
@@ -24,6 +33,7 @@ def upload_image():
     with open(file_path, 'wb') as f:
         f.write(image_bytes)
 
+    print('finish', finish)
     return jsonify({'message': '画像が保存されました'}), 200
 
 if __name__ == '__main__':
